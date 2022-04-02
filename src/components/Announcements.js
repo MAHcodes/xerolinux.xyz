42a06be708b8f278391d0c865dd777444bd1c7b4
mhmdali102 <mhmdali102@bk.ru>
2022-04-02 21:13:34 +0300

    announcement section

---
+++ src/components/Announcements.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import Button from "./Button";
+
+const Announcements = () => {
+  const announce = () => {
+    console.log("POPUP");
+  };
+
+  return (
+    <Div>
+      <div className="container">
+        <P>Major Project Announcements</P>
+        <Button primary text="read more" action={announce} />
+      </div>
+    </Div>
+  );
+};
+
+const Div = styled.div`
+  background-color: rgb(var(--important));
+  padding: 1rem;
+
+  & .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
+    gap: 0.5em;
+    text-align: center;
+  }
+`;
+
+const P = styled.p`
+  margin-inline-end: 1em;
+  color: rgb(var(--white));
+  font-size: var(--fz-6);
+`;
+
+export default Announcements;
