e5f57fdccd96b3134b7fbf6ceb6c52d75270684c
mhmdali102 <mhmdali102@bk.ru>
2022-04-03 03:08:29 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import Announcements from "./components/Announcements";
 import Header from "./components/Header";
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
-      <Announcements />
-      <Header />
+      <Wrapper>
+        <Announcements />
+        <Header />
+      </Wrapper>
     </div>
   );
 }
+
+const Wrapper = styled.div`
+  position: sticky;
+  inset: 0 0 auto;
+`;
 
 export default App;
