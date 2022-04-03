2042d51ebe9a33540c0d719fd7e0bfee9f0d3e42
mhmdali102 <mhmdali102@bk.ru>
2022-04-03 22:59:27 +0300

    fix eslint warning

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,4 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import laptop from "../assets/images/laptop.png";
 import Button from "./Button";
 import leb from "../assets/images/leb.svg";
