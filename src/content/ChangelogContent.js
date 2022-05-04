d6564234615400be3012a9bb293d32d5bfd18489
XeroLinux <steve@techxero.com>
2022-05-04 21:51:48 +0300

    Update

--- src/content/ChangelogContent.js
+++ src/content/ChangelogContent.js
@@ -6,44 +6,38 @@
 
 <p>Below is a list of changes since the last release. This one will require a <strong>Fresh Install</strong> to benefit from all the latest &amp; greatest updates. However, if you are not interested in any of them, and you are currently happy with your current <strong>XeroLinux</strong> setup, you can
 keep using it with no interruptions.
-<br /> <br />
-This will be the last time this will be required, as from this release
-onwards, it will only be package update ISOs with Distro specific ones
-delivered via my tool. So to know when you will need to use that new
-feature, keep coming back here or join our <strong>Discord/Telegram</strong>
-…</p>
-<p>Now, without further ado, here are the latest changes :</p>
+<br />
+</p>
+<h4>- Changelog 25/05/2022 :</h4>
+<br />
 <ul>
+<li><strong>Calamares</strong> updated to version 3.2.57.</li>
+<li><strong>YaY</strong> added alongside <strong>Paru</strong>.</li>
 <li>New revamped tool now called <strong>XeroTool</strong>.</li>
 <li>New <strong>XeroLinux XL</strong> Repo for larger packages.</li>
 <li>No more <strong>ArcoLinux</strong> Repos. Just ours.</li>
 <li><strong>PipeWire</strong> is now the default Audio Server.</li>
 <li>Default FileSystem is now <strong>XFS</strong> vs <strong>BTRFS.</strong></li>
 <li>No more Package Selection In <strong>Calamares</strong>.</li>
-<li>New <strong>BTRFS/Snapper</strong> tools in case you use that FS.**</li>
+<li>New <strong>BTRFS/Snapper</strong> tools in case you use that FS.*</li>
 <li>New Post Install Package Installer (WiP).</li>
 <li>Tweaked <strong>SDDM</strong> Config for Optimus users.</li>
 <li>New Button in <strong>Calamares</strong> to follow along during install.</li>
-<li>All <strong>GPU</strong> Drivers moved to Calamares no longer on ISO.</li>
+<li>All <strong>GPU</strong> Drivers moved to Calamares no longer on ISO.**</li>
 <li>Less boot options on <strong>USB Boot</strong>, no more confusion.</li>
 <li>New <strong>Rice Switcher</strong> via the new <strong>XeroTool</strong>.</li>
 <li>Tons of under-the-hood tweaks &amp; optimizations.</li>
 <li>Tweaked the default Rice making it better.</li>
 <li>New Aliases added to both .bashrc &amp; .zshrc.</li>
-<li><strong>YaY</strong> added alongside <strong>Paru</strong>.</li>
 <li><strong>X11</strong> Session is the default post install.</li>
-<li><strong>Pamac GUI</strong> replaced by <strong>Bauh GUI</strong></li>
+<li><strong>Snaps</strong> Support removed from <strong>Pamac.</strong></li>
+<li><strong>AutoComposer</strong> added for better gaming experience.<br>
+Thanks to <strong><i>GamerKing64</i></strong> for the suggestion.</li>
 </ul>
 <details open>
 <summary>Notes Please read...</summary>
 <p class="bg">
-* : Make sure to watch the new
-<span> “Complete XeroLinux Install Guide” </span>
-video attached in the “Download” section for more detail on that &
-more….
-</p>
-<p class="bg">
-** : Make sure to go over the new <span>BTRFS-Assistant</span> &
+* : Make sure to go over the new <span>BTRFS-Assistant</span> &
 <span> Snapper tool </span>
 thread for more information on that over on the =>
 <a
@@ -53,6 +47,12 @@ rel="noreferrer"
 >
 <span> XeroLinux Forums…</span>
 </a>
+</p>
+<p class="bg">
+** : Make sure to watch the new
+<span> “Complete XeroLinux Install Guide” </span>
+video attached in the “Download” section for more detail on that &
+more….
 </p>
 </details>
 
