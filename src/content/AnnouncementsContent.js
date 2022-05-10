bd55ed3d95833cfe470dd745715fb2a9e2d48a2c
XeroLinux <steve@techxero.com>
2022-05-10 20:12:07 +0300

    Subs

--- src/content/AnnouncementsContent.js
+++ src/content/AnnouncementsContent.js
@@ -3,13 +3,13 @@
             <div>
             <h1 id="dear-xerolinux-users">Dear XeroLinux users,</h1><br />
 <p>Kindly find the latest news &amp; announcements below… Read carefully.</p>
-<h5 id="xfce--gnome-discontinued-">- XFCE &amp; GNOME Discontinued :</h5><br />
+<h4 id="xfce--gnome-discontinued-">- XFCE &amp; GNOME Discontinued :</h4><br />
 <p>Starting May 25th, moving forward, there will only be <strong>KDE</strong> version. <strong>XFCE</strong> &amp; <strong>GNOME</strong> have been deleted. No manpower or time. </p>
-<h5 id="repos-have-been-moved-">- Repos Have been moved :</h5><br />
+<h4 id="repos-have-been-moved-">- Repos Have been moved :</h4><br />
 <p>Since switching away from <strong>&quot;ArcoLinux&quot;</strong> repositories, and moving our own from one server to another, that, caused issues for some with older versions of <strong>XeroLinux</strong>.. Solution in the FAQ section…</p>
-<h5 id="repos-have-been-moved-">- Wayland Support Removed from ISO :</h5><br />
+<h4 id="repos-have-been-moved-">- Wayland Support Removed from ISO :</h4><br />
 <p>I have decided to remove <span>Wayland</span> support from ISO, as it's not yet ready to be used on a daily... It has been moved to the <span>XeroLinux Tool</span>, under the "Fix Common Issues" menu, if you insist on testing it out and have an <span>AMD GPU</span>...</p>
-<h5 id="repos-have-been-moved-">- Final Major Release :</h5><br />
+<h4 id="repos-have-been-moved-">- Final Major Release :</h4><br />
 <p>As of May 25th after release is public, I will only be here to provide support when and if I can, whilst keeping repos up to date...</p>
 <p>No more releases after that unless it's required for fixing stuff.. I will be traveling a lot this summer to find work that actually pays the rent...</p>
 <p>Thank you all for your continued support ;)</p>
