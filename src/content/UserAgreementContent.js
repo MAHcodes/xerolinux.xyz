42745fff4f719311e502d756fa69009beb917982
XeroLinux <steve@techxero.com>
2022-05-14 00:29:03 +0300

    Points

--- src/content/UserAgreementContent.js
+++ src/content/UserAgreementContent.js
@@ -4,9 +4,9 @@
 <p>Please read this carefully to understand more about what this project is and what it entails.</p>
 <h1 id="-eol-"><center>~~~~~~~~~~~~ S.O.F ~~~~~~~~~~~~</center></h1>
 <h4 id="troubleshooting">- Main Points.</h4><br />
-<p>(1) This is a <strong>Hobby distribution</strong>, official support not guaranteed.</p>
-<p>(2) Provides third-party repositories and packages for functionality.</p>
-<p>(3) No companies involved in creation of Distro-specific packages and tweaks.</p>
+<p><strong>(1)</strong> This is a <strong>Hobby distribution</strong>, official support not guaranteed.</p>
+<p><strong>(2)</strong> Provides third-party repositories and packages for functionality.</p>
+<p><strong>(3)</strong> No companies involved in creation of Distro-specific packages and tweaks.</p>
 <h4 id="troubleshooting">- Troubleshooting.</h4><br />
 <p>When it comes to issues related to your specific hardware config, use the awesome knowledge found on <a href="https://wiki.archlinux.org/"><strong>ArchWiki</strong></a>.</p>
 <p>Otherwise, we will do best we can to assist you. If not myself, other users might be able to chime in if and when they can. </p>
