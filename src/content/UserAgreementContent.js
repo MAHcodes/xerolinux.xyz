c132cd7fdfb77f0b8ba8280766194367ea7a121e
XeroLinux <steve@techxero.com>
2022-05-13 23:59:21 +0300

    Update

--- src/content/UserAgreementContent.js
+++ src/content/UserAgreementContent.js
@@ -4,12 +4,10 @@
 <p>Please read this carefully to understand more about what this project is and what it entails.</p>
 <h4 id="troubleshooting">- Main Points.</h4><br />
 <p>(1) This is a <strong>Hobby distribution</strong>, formal support not guaranteed.</p>
-<p>(2) XeroLinux-specific packages and code modifications have been created by end-user individuals. There are no companies officially involved.</p>
-<p>(3) Consider it as a solid, well optimized &amp; great looking Arch/KDE base to build on top of.</p>
-<p>(4) This distribution provides third-party repositories and packages which are already enabled and installed for functionality.</p>
+<p>(2) No companies officially involved in creation of XeroLinux-specific packages and code modifications.</p>
+<p>(3) Provides third-party repositories and packages which are already enabled and installed for functionality.</p>
 <h4 id="troubleshooting">- Troubleshooting.</h4><br />
-<p>When it comes to issues related to your specific hardware config, use the awesome knowledge found on <a href="https://wiki.archlinux.org/"><strong>ArchWiki</strong></a>.</p>
-<p>Otherwise, we will do best we can to assist you. If not myself, other users might be able to chime in if and when they can. </p>
+<p>When it comes to issues related to your specific hardware config, use the awesome knowledge found on <a href="https://wiki.archlinux.org/"><strong>ArchWiki</strong></a>. Otherwise, we will do best we can to assist you.</p>
 <h1 id="-eol-"><center>~~~~~~~~~~~~ E.O.L ~~~~~~~~~~~~</center></h1>
 
             </div>
