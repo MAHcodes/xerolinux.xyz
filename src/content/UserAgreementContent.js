d20a55eec1d9d475a3cd669741129652229ec52a
XeroLinux <steve@techxero.com>
2022-05-13 23:54:00 +0300

    Agreement

--- src/content/UserAgreementContent.js
+++ src/content/UserAgreementContent.js
@@ -3,14 +3,13 @@
             <div>
 <p>Please read this carefully to understand more about what this project is and what it entails.</p>
 <h4 id="troubleshooting">- Main Points.</h4><br />
-<p>(1) You understand this is a <strong>Hobby distribution</strong>, it is not to be used in production environments. By downloading, installing, and using this distribution, you agree that you understand you are not entitled to receive any kind of formal support.</p>
-<p>(2) You understand all XeroLinux-specific packages and code modifications have been created by end-user individuals. There are no companies officially involved.</p>
-<p>(3) You understand it was never intended to be everything for everyone. Consider it as a solid, well optimized &amp; great looking Arch/KDE base to build on top of, that&#39;s what makes <strong>Linux</strong> so damn awesome !</p>
+<p>(1) This is a <strong>Hobby distribution</strong>, formal support not guaranteed.</p>
+<p>(2) XeroLinux-specific packages and code modifications have been created by end-user individuals. There are no companies officially involved.</p>
+<p>(3) Consider it as a solid, well optimized &amp; great looking Arch/KDE base to build on top of.</p>
 <p>(4) This distribution provides third-party repositories and packages which are already enabled and installed for functionality.</p>
-<p>By downloading, installing, and using this distribution, you agree that you understand these 3rd party repositories are installed and enabled, and that this distribution ships with some of these packages installed.</p>
 <h4 id="troubleshooting">- Troubleshooting.</h4><br />
-<p>When it comes to issues related to your specific hardware config, you better be ready to do your own research using the awesome knowledge found on <a href="https://wiki.archlinux.org/"><strong>ArchWiki</strong></a> and/or the Web. You can also help others by sharing solutions, that&#39;s the power of the community.</p>
-<p>Otherwise, we will do best we can to assist you. If not myself, other users might be able to chime in if and when they can. All I ask for is a lot of patience as I am one man doing this, not a team.</p>
+<p>When it comes to issues related to your specific hardware config, use the awesome knowledge found on <a href="https://wiki.archlinux.org/"><strong>ArchWiki</strong></a>.</p>
+<p>Otherwise, we will do best we can to assist you. If not myself, other users might be able to chime in if and when they can. </p>
 <h1 id="-eol-"><center>~~~~~~~~~~~~ E.O.L ~~~~~~~~~~~~</center></h1>
 
             </div>
