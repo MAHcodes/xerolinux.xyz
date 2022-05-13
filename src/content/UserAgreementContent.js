cf3bca24c097cb686a238ab86d432bdf8040cc1c
XeroLinux <steve@techxero.com>
2022-05-13 22:39:36 +0300

    Update

--- src/content/UserAgreementContent.js
+++ src/content/UserAgreementContent.js
@@ -2,10 +2,12 @@
         export const html = `
             <div>
 <p>Please read this carefully to understand more about what this project is and what it entails.</p>
-<p>This Distro is a <strong>private</strong> project that I have created for myself first and foremost, as a hobby, in my spare time, to learn the inner workings of Linux, which means it&#39;s mainly compatible with the hardware I currently own...</p>
-<p>It was never intended to be everything for everyone. Consider it as a solid, well optimized &amp; great looking Arch/KDE base to build on top of, that&#39;s what makes <strong>Linux</strong> so damn awesome !</p>
-<h4 id="target-audience">- Target Audience.</h4><br />
-<p>Do you love <strong>Arch BTW</strong> &amp; <strong>KDE</strong> as much as I do ? Are you a Linux <strong>Nerd</strong> like me ? And do you love to learn and teach ? If yes, then this Distro is for you.</p>
+<h4 id="troubleshooting">- Main Points.</h4><br />
+<p>(1) You understand this is a <strong>Hobby distribution</strong>, it is not to be used in production environments. By downloading, installing, and using this distribution, you agree that you understand you are not entitled to receive any kind of formal support.</p>
+<p>(2) You understand all Nobara-specific packages and code modifications have been created by end-user individuals. There are no companies officially involved.</p>
+<p>(3) You understand it was never intended to be everything for everyone. Consider it as a solid, well optimized &amp; great looking Arch/KDE base to build on top of, that&#39;s what makes <strong>Linux</strong> so damn awesome !</p>
+<p>(4) This distribution provides third-party repositories and packages which are already enabled and installed for functionality.</p>
+<p>By downloading, installing, and using this distribution, you agree that you understand these 3rd party repositories are installed and enabled, and that this distribution ships with some of these packages installed.</p>
 <h4 id="troubleshooting">- Troubleshooting.</h4><br />
 <p>When it comes to issues related to your specific hardware config, you better be ready to do your own research using the awesome knowledge found on <a href="https://wiki.archlinux.org/"><strong>ArchWiki</strong></a> and/or the Web. You can also help others by sharing solutions, that&#39;s the power of the community.</p>
 <p>Otherwise, we will do best we can to assist you. If not myself, other users might be able to chime in if and when they can. All I ask for is a lot of patience as I am one man doing this, not a team.</p>
@@ -13,4 +15,3 @@
 
             </div>
         `
-    
