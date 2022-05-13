014fa23d82cc227f37caa98e43229ea6864d3d41
XeroLinux <steve@techxero.com>
2022-05-14 00:20:25 +0300

    Fix

--- src/content/UserAgreementContent.js
+++ src/content/UserAgreementContent.js
@@ -2,15 +2,15 @@
         export const html = `
             <div>
 <p>Please read this carefully to understand more about what this project is and what it entails.</p>
-<h1 id="-eol-"><center>~~~~~~~~~~~~ S.O.L ~~~~~~~~~~~~</center></h1>
+<h1 id="-eol-"><center>~~~~~~~~~~~~ S.O.F ~~~~~~~~~~~~</center></h1>
 <h4 id="troubleshooting">- Main Points.</h4><br />
 <p>(1) This is a <strong>Hobby distribution</strong>, official support not guaranteed.</p>
-<p>(2) No companies officially involved in creation of XeroLinux-specific packages and tweaks.</p>
-<p>(3) Provides third-party repositories and packages for functionality.</p>
+<p>(2) Provides third-party repositories and packages for functionality.</p>
+<p>(3) No companies involved in creation of Distro-specific packages and tweaks.</p>
 <h4 id="troubleshooting">- Troubleshooting.</h4><br />
 <p>When it comes to issues related to your specific hardware config, use the awesome knowledge found on <a href="https://wiki.archlinux.org/"><strong>ArchWiki</strong></a>.</p>
 <p>Otherwise, we will do best we can to assist you. If not myself, other users might be able to chime in if and when they can. </p>
-<h1 id="-eol-"><center>~~~~~~~~~~~~ E.O.L ~~~~~~~~~~~~</center></h1>
+<h1 id="-eol-"><center>~~~~~~~~~~~~ E.O.F ~~~~~~~~~~~~</center></h1>
 
             </div>
         `
