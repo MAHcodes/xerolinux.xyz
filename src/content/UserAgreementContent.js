d0a9e58b2d27d211c6ec84f42d0aeacb7d201c3d
XeroLinux <steve@techxero.com>
2022-05-13 22:41:54 +0300

    Nobara

--- src/content/UserAgreementContent.js
+++ src/content/UserAgreementContent.js
@@ -4,7 +4,7 @@
 <p>Please read this carefully to understand more about what this project is and what it entails.</p>
 <h4 id="troubleshooting">- Main Points.</h4><br />
 <p>(1) You understand this is a <strong>Hobby distribution</strong>, it is not to be used in production environments. By downloading, installing, and using this distribution, you agree that you understand you are not entitled to receive any kind of formal support.</p>
-<p>(2) You understand all Nobara-specific packages and code modifications have been created by end-user individuals. There are no companies officially involved.</p>
+<p>(2) You understand all XeroLinux-specific packages and code modifications have been created by end-user individuals. There are no companies officially involved.</p>
 <p>(3) You understand it was never intended to be everything for everyone. Consider it as a solid, well optimized &amp; great looking Arch/KDE base to build on top of, that&#39;s what makes <strong>Linux</strong> so damn awesome !</p>
 <p>(4) This distribution provides third-party repositories and packages which are already enabled and installed for functionality.</p>
 <p>By downloading, installing, and using this distribution, you agree that you understand these 3rd party repositories are installed and enabled, and that this distribution ships with some of these packages installed.</p>
