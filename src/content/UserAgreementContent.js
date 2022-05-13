8b515eec9ea82cca2b61beb9b74daef053a1a076
XeroLinux <steve@techxero.com>
2022-05-14 00:05:05 +0300

    Updates

--- src/content/UserAgreementContent.js
+++ src/content/UserAgreementContent.js
@@ -2,10 +2,11 @@
         export const html = `
             <div>
 <p>Please read this carefully to understand more about what this project is and what it entails.</p>
+<h1 id="-eol-"><center>~~~~~~~~~~~~ S.O.L ~~~~~~~~~~~~</center></h1>
 <h4 id="troubleshooting">- Main Points.</h4><br />
-<p>(1) This is a <strong>Hobby distribution</strong>, formal support not guaranteed.</p>
-<p>(2) No companies officially involved in creation of XeroLinux-specific packages and code modifications.</p>
-<p>(3) Provides third-party repositories and packages which are already enabled and installed for functionality.</p>
+<p>(1) This is a <strong>Hobby distribution</strong>, official support not guaranteed.</p>
+<p>(2) No companies officially involved in creation of XeroLinux-specific packages and tweaks.</p>
+<p>(3) Provides third-party repositories and packages for functionality.</p>
 <h4 id="troubleshooting">- Troubleshooting.</h4><br />
 <p>When it comes to issues related to your specific hardware config, use the awesome knowledge found on <a href="https://wiki.archlinux.org/"><strong>ArchWiki</strong></a>.</p>
 <p>Otherwise, we will do best we can to assist you. If not myself, other users might be able to chime in if and when they can. </p>
