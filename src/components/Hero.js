eee3363fece26f8b5e1049681bde21021edcb254
mhmdali102 <mhmdali102@bk.ru>
2022-05-15 17:09:14 +0300

    my pcbuild new design

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -80,12 +80,24 @@ const Hero = ({ countdown }) => {
             <SocialLinks clr="rgb(var(--white))" />
           </Content>
           <MoreWrapper>
-              <Button
-                text="My PC Build"
-                action={popUpPcBuild}
-                clr="rgb(var(--white))"
-                padding=".75em 1.25em"
-              />
+            <PCBuild>
+              <p>
+                My PC Build
+                <svg
+                  width="280"
+                  height="103"
+                  viewBox="0 0 280 103"
+                  fill="none"
+                  onClick={popUpPcBuild}
+                >
+                  <path
+                    d="M75.11 27.8295H74.4886L74.0493 28.2688L14.7151 87.603C13.3116 86.298 11.4303 85.4999 9.36264 85.4999C5.02023 85.4999 1.5 89.0201 1.5 93.3626C1.5 97.705 5.02022 101.225 9.36264 101.225C13.7051 101.225 17.2253 97.705 17.2253 93.3626C17.2253 91.5875 16.6371 89.9499 15.645 88.6339L16.7859 89.7748L75.7313 30.8295H104.363V53.4174V54.9174H105.863H111.65L110.507 56.0606L117.589 63.1429L118.028 63.5823H118.649H182.276V61.9628L183.275 62.9616L183.714 63.401H184.335H276.594H278.094V61.901V11.6042V10.1042H276.594H249.192L249.935 9.3631L242.523 1.9402L242.083 1.50006H241.461H176.913V2.06027L176.912 2.06003L176.473 1.62069H175.852H105.863H104.363V3.12069V27.8295H75.11ZM119.271 60.5823L113.606 54.9174H175.23L180.895 60.5823H119.271ZM246.435 10.1042H184.957L179.353 4.50006H240.839L246.435 10.1042Z"
+                    stroke="#FFCFF2"
+                    strokeWidth="3"
+                  />
+                </svg>
+              </p>
+            </PCBuild>
             <ImgContainer>
               <Weather />
               <img src={laptop} alt="XeroLinux" />
@@ -133,6 +145,40 @@ from {
 const Content = styled.div`
   flex: 0.5;
   animation: ${ContentIn} 0.5s var(--transition-timing-function);
+`;
+
+const PCBuild = styled.div`
+  cursor: pointer;
+  transform: scale(.9);
+  & > p {
+    position: relative;
+    font-weight: bold;
+    margin-inline: auto;
+    width: fit-content;
+    top: -2.5rem;
+    left: 35%;
+  }
+
+  svg {
+    position: absolute;
+    height: 350%;
+    width: 300%;
+    inset: -.85rem -3.25rem auto auto;
+    cursor: pointer;
+    mix-blend-mode: lighten;
+    transition: fill var(--transition-duration) var(--transition-timing-function);
+
+    & path {
+      transition: stroke var(--transition-duration) var(--transition-timing-function);
+    }
+
+    &:hover {
+      fill: rgba(105, 110, 194, 40%);
+      & path {
+        stroke: rgb(105, 110, 194); 
+      }
+    }
+  }
 `;
 
 const Origin = styled.div`
