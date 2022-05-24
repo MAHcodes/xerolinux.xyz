61b9569240b47366c781d2e4608b44d0cba8f873
XeroLinux <steve@techxero.com>
2022-05-24 12:33:37 +0300

    EULA

--- src/content/UserAgreementContent.js
+++ src/content/UserAgreementContent.js
@@ -5,11 +5,12 @@
 <h1 id="-eol-"><center>~~~~~~~~~~~~ S.O.F ~~~~~~~~~~~~</center></h1>
 <h4 id="troubleshooting">- Main Points.</h4><br />
 <p><strong>(1)</strong> This is a <strong>Hobby distribution</strong>, issue resolution not always guaranteed.</p>
-<p><strong>(2)</strong> Provides third-party repositories and packages for functionality.</p>
+<p><strong>(2)</strong> Provides third-party repositories and packages for better functionality.</p>
 <p><strong>(3)</strong> No companies involved in creation of Distro-specific packages and tweaks.</p>
 <h4 id="troubleshooting">- Troubleshooting.</h4><br />
 <p>When it comes to issues related to your specific hardware config, use the awesome knowledge found on <a href="https://wiki.archlinux.org/"><strong>ArchWiki</strong></a>.</p>
-<p>We cannot be held responsible in case of Data-Loss/Corruption. Do your research, test in a VM before taking the plunge on real metal.</p>
+<h4><center><strong>~~ Important Note ~~</strong></center></h4><br />
+<p>This distro is 90% <strong>ArchLinux</strong> & 10% custom. Also, we cannot be held responsible in case of Data-Loss or Corruption. Do your research, test in a VM before taking the plunge on real metal.</p>
 <h1 id="-eol-"><center>~~~~~~~~~~~~ E.O.F ~~~~~~~~~~~~</center></h1>
 
             </div>
