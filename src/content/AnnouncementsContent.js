b1c2b39cafbfdb81dabfa85520ae7f7fb6855bde
XeroLinux <steve@techxero.com>
2022-06-10 12:25:21 +0300

    Updates

--- src/content/AnnouncementsContent.js
+++ src/content/AnnouncementsContent.js
@@ -5,12 +5,12 @@
 <p>Kindly find the latest news &amp; announcements below… Read carefully.</p>
 <h4 id="xfce--gnome-discontinued-">- One DE to rule them All :</h4><br />
 <p>Starting May 25th, moving forward, there will only be <strong>KDE</strong> version. </p>
-<h4 id="repos-have-been-moved-">- Repos Have been moved :</h4><br />
+<h4 id="repos-have-been-moved-">- ArcoLinux Repos Have been removed :</h4><br />
 <p>We have switched away from <strong>&quot;ArcoLinux&quot;</strong> repositories to our own...</p>
 <h4 id="repos-have-been-moved-">- Wayland Support Removed from ISO :</h4><br />
-<p><span>Wayland</span> support removed from ISO, moved to the <span>XeroLinux Tool</span>, under the "Fix Common Issues" menu.</p>
+<p><span>Wayland</span> support removed from ISO, moved to the <span>XeroLinux Tool</span>.</p>
 <h4 id="repos-have-been-moved-">- Final Major Release :</h4><br />
-<p>As of May 25th, release schedule will switch to <strong>&quot;When it's Needed&quot;</strong> style. No more monthly ISOs, Will announce new releases when and if there are any...</p>
+<p>As of May 25th, release schedule will switch to <strong>&quot;When it's Needed&quot;</strong> style. No more monthly ISOs, we will announce new releases when and if there are any...</p>
 <p>Thank you all for your continued support ;)</p>
 
             </div>
