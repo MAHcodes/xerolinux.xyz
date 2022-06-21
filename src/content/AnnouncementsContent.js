619a7a830cf578a49539a13ab522e097922b2e48
XeroLinux <steve@techxero.com>
2022-06-21 18:08:46 +0300

    Updates

--- src/content/AnnouncementsContent.js
+++ src/content/AnnouncementsContent.js
@@ -1,16 +1,16 @@
 
         export const html = `
             <div>
-            <h1 id="dear-xerolinux-users">Dear XeroLinux users,</h1><br />
+            <center><h1 id="dear-xerolinux-users">Dear XeroLinux users,</h1></center><br />
 <p>Kindly find the latest news &amp; announcements below… Read carefully.</p>
 <h4 id="xfce--gnome-discontinued-">- One DE to rule them All :</h4><br />
-<p>Moving forward, there will only be <strong>KDE Plasma</strong> version. Soon we will include Arco's all new <span>ArchLinux Tweak Tool</span> or "ATT" for short, in case you want to add new WMs/DEs. Support for that will be provided by them only, not us...</p></p>
+<p>Moving forward, there will only be <strong>KDE Plasma</strong> version, and nothing else. But if you want to use another DE or WM/TWM, soon Arco's all new  <a href="https://www.youtube.com/watch?v=MMbZ1Jh9J1M" target="_blank" rel="noreferrer"> <span> ArchLinux Tweak Tool</span></a> or "ATT" for short, will allow you to so. and much more. Support for that will be provided by them only, not us...</p></p>
 <h4 id="repos-have-been-moved-">- ArcoLinux Repos Have been removed :</h4><br />
-<p>We have switched away from <strong>&quot;ArcoLinux&quot;</strong> repositories to our own.. You can, however add them back, if you so wish via their <a href="https://www.arcolinux.info/arcolinux-spices-application/" target="_blank" rel="noreferrer"> <span> Spices Tool</span></a>...</p>
+<p>We have switched away from <strong>&quot;ArcoLinux&quot;</strong> repositories to our own.. You can, however add them back, if you so wish via their <a href="https://www.arcolinux.info/arcolinux-spices-application/" target="_blank" rel="noreferrer"> <span> Spices Tool</span></a>... However, keep this in mind before attempting to do so, <span>BEWARE OF MIXING REPOSITORIES !!</span></p>
 <h4 id="repos-have-been-moved-">- Stability vs Features :</h4><br />
-<p>We are now aiming more towards <span>Stability</span> & <span>Consistency</span> over features. So new features will be few and far between, while we concentrate on making you experience with <span>XeroLinux</span> a pleasant one.</p>
+<p>We are now aiming more towards <span>Stability</span> & <span>Consistency</span> over features. So new features will be few and far between, while we concentrate on making your experience with <span>XeroLinux</span> a pleasant one.</p>
 <h4 id="repos-have-been-moved-">- No more "Monthly" ISOs :</h4><br />
-<p>As of now, release schedule will switch to <span>"Quarterly"</span> as in every 3 months. No more monthly ISOs. It might happen the we skip a release due to unforeseen problems, if that happens will let you know...</p>
+<p>As of now, release schedule will switch to <span>"Quarterly"</span> as in every 3 months. No more monthly ISOs. In case of delays, for whatever reason, we will let you know...</p>
 <p>Thank you all for your continued support ;)</p>
 
             </div>
--- src/content/ChangelogContent.js
+++ src/content/ChangelogContent.js
@@ -12,7 +12,7 @@
 <h4>- Changelog 16/06/2022 :</h4>
 <br />
 <ul>
-<li><strong>KDE Plasma</strong> updated to 5.25.0 initial release expect bugs.</li>
+<li><strong>KDE Plasma</strong> updated to 5.25.0 initial release expect bugs.(*)</li>
 <li><strong>KDE Frameworks</strong> updated to 5.95 including all packages.</li>
 <li>New <strong>QT6 Libraries</strong> added and <strong>QT5 Libraries</strong> updated to latest.</li>
 <li><strong>Floating Panel</strong> enabled by default. Will be applied to all rices soon.</li>
@@ -33,15 +33,15 @@
 <li>New <strong>BTRFS/Snapper</strong> tools in case you use that FS.</li>
 <li>New Post Install Package Installer (via tool).</li>
 <li>Tweaked <strong>SDDM</strong> Config for Optimus users.</li>
-<li><strong>nVidia-dkms-tkg</strong> Driver Stack used instead of Official.(*)</li>
+<li><strong>nVidia-dkms-tkg</strong> Driver Stack used instead of Official.(**)</li>
 <li>New Button in <strong>Calamares</strong> to follow along during install.</li>
-<li>All <strong>GPU</strong> Drivers moved to Calamares no longer on ISO.**</li>
+<li>All <strong>GPU</strong> Drivers moved to Calamares no longer on ISO.</li>
 <li>Less boot options on <strong>USB Boot</strong>, no more confusion.</li>
 <li>New <strong>Rice Switcher</strong> via the new <strong>XeroTool</strong>.</li>
 <li>Tons of under-the-hood tweaks &amp; optimizations.</li>
 <li>Tweaked the default Rice making it better.</li>
 <li>New simple <strong>Grub</strong> theme included.</li>
-<li><strong>Wayland</strong> Session removed from ISO.***</li>
+<li><strong>Wayland</strong> Session removed from ISO.(***)</li>
 <li><strong>Snaps</strong> Support removed from <strong>Pamac.</strong></li>
 <li><strong>AutoComposer</strong> added for better gaming experience.<br></li>
 <li>Added <strong>XeroTool</strong> icon to System Tray for easy access.</li>
@@ -49,7 +49,16 @@
 <details open>
 <summary>Notes Please read...</summary>
 <p class="bg">
-* : We opted to use the <span>TKG</span> patched versions of the 515 nVidia driver stack. We will stick with those from here on in, as they preform better & are more stable than the Official ones. Available from our new <span>nVidia Repo</span>.
+* : Make sure to go over this thread to know more about the current situation with => <a
+href="https://forum.xerolinux.xyz/thread-137.html"
+target="_blank"
+rel="noreferrer"
+>
+<span> Plasma 5.25 Realease…</span>
+</a>.
+</p>
+<p class="bg">
+** : We opted to use the <span>TKG</span> patched versions of the 515 nVidia driver stack. We will stick with those from here on in, as they preform better & are more stable than the Official ones. Available from our new <span>nVidia Repo</span>.
 <a
 href="https://forum.xerolinux.xyz/thread-124.html"
 target="_blank"
@@ -57,17 +66,6 @@ rel="noreferrer"
 >
 <span> nVidia Driver Troubleshooting</span>
 </a>
-</p>
-<p class="bg">
-** : Make sure to go over the new
-<span> “Complete XeroLinux Install Guide” </span>
-thread on the => <a
-href="https://forum.xerolinux.xyz/thread-38.html"
-target="_blank"
-rel="noreferrer"
->
-<span> XeroLinux Forums…</span>
-</a>.
 </p>
 <p class="bg">
 *** : You will be able to add
