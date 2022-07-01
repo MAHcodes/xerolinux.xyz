59af46ac380b5a9c7019d5edfcb981aae4b43246
XeroLinux <steve@techxero.com>
2022-07-01 18:25:59 +0300

    Updates

--- src/content/UserAgreementContent.js
+++ src/content/UserAgreementContent.js
@@ -1,17 +1,19 @@
 
         export const html = `
             <div>
-<p>Please read this carefully to understand more about what this project is and what it entails.</p>
-<h1 id="-eol-"><center>~~~~~~~~~~~~ S.O.F ~~~~~~~~~~~~</center></h1>
+<p>Please read this carefully to understand more about what this project is and what it entails. Setting expectations</p>
+<h1 id="-eol-"><center>~~~~~~~~~~~~ E.U.A ~~~~~~~~~~~~</center></h1>
 <h4 id="troubleshooting">- Main Points.</h4><br />
-<p><strong>(1)</strong> This is a <strong>Hobby distribution</strong>, issue resolution not always guaranteed.</p>
-<p><strong>(2)</strong> Provides third-party repositories and packages for better functionality.</p>
-<p><strong>(3)</strong> No companies involved in creation of Distro-specific packages and tweaks.</p>
+<p><strong>(1)</strong> <strong>XeroLinux</strong> is a <strong>Hobby distribution</strong>, not a job, issue resolution not always guaranteed. But we will do our best. It just offers an easy way to install <strong>ArchLinux</strong> with a well optimized & Customized <strong>KDE Plasma</strong>, after which you will be able to shape it YOUR way via our tool.</p>
+<p><strong>(2)</strong> We provide our own custom repositories and packages for better functionality. You can add your own later on.</p>
+<p><strong>(3)</strong> <strong>XeroLinux</strong> is not a full-on Distro by any stretch of the imagination. Nothing is nor will ever be forced on you, the whole point of it is to provide you with a stable base that you can shape YOUR way, the easy way...</p>
 <h4 id="troubleshooting">- Troubleshooting.</h4><br />
-<p>When it comes to issues related to your specific hardware config, use the awesome knowledge found on <a href="https://wiki.archlinux.org/"><strong>ArchWiki</strong></a>.</p>
+<p>When it comes to issues related to your specific hardware config, and you don't know if your PC or Laptop is supported, we have taken the much safer route of leaving the issue resolution up to you, the user, since you are the only one who can test the solutions on your hardware. Kindly either post your questions on <a href="https://www.linuxquestions.org/questions/" target="_blank"><strong>LinuxQuestions</strong></a> after having searched on their fourms to see if your issue has ben addressed already or not, or use the awesome knowledge found on <a href="https://wiki.archlinux.org/" target="_blank"><strong>ArchWiki</strong></a>, it's all Arch in the end. If you still can't find solutions for your issues, why not look for them using your Seach Engine of choice ?</p>
 <h4><center><strong>~~ Important Note ~~</strong></center></h4><br />
 <p>This distro is 90% <strong>ArchLinux</strong> & 10% custom. Also, we cannot be held responsible in case of Data-Loss or Corruption. Do your research, test in a VM before taking the plunge on real metal.</p>
-<h1 id="-eol-"><center>~~~~~~~~~~~~ E.O.F ~~~~~~~~~~~~</center></h1>
+<p><center><strong>!!! Do NOT Download Unless You Have Read & Agree with The Above !!!<br>
+If you download without reading, and have issues, it's on you...</strong></center></p>
+<h1 id="-eol-"><center>~~~~~~~~~~~~ E.U.A ~~~~~~~~~~~~</center></h1>
 
             </div>
         `
