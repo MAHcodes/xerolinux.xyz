432b5487722b8b18f1651fc85469491dce712484
mhmdali102 <mhmdali102@bk.ru>
2022-07-10 03:27:17 +0300

    HeroContent & FeaturesContent

--- src/components/Features.js
+++ src/components/Features.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Box from "./Box";
 import Card from "./Card";
 import Title from "./Title";
+import { CONTENT } from "../content/FeaturesContent";
 
 const Features = () => {
   return (
@@ -37,8 +38,8 @@ const Features = () => {
                   />
                 </svg>
               }
-              title="Improved"
-              text="XeroLinux runs fast due to the many, many fixes and under the hood tweaks that will save you time and provide you with a better & smoother experience"
+              title={CONTENT[0].title}
+              text={CONTENT[0].text}
             />
             <Card
               icon={
@@ -55,8 +56,8 @@ const Features = () => {
                   />
                 </svg>
               }
-              title={"Stunning"}
-              text="XeroLinux looks Stunning out of the box. Using almost everything at our disposal from KDE making it look sexy without compromising performance. We also offer many alternate rices as well."
+              title={CONTENT[1].title}
+              text={CONTENT[1].text}
             />
             <Card
               icon={
@@ -73,8 +74,8 @@ const Features = () => {
                   />
                 </svg>
               }
-              title="Powerful"
-              text="With built-in support for AUR and Flatpaks, as well as the topgrade T.U.I updater, XeroLinux gives you the power to shape your Linux system YOUR way, the easy way."
+              title={CONTENT[2].title}
+              text={CONTENT[2].text}
             />
           </Cards>
         </Box>
--- src/content/FeaturesContent.js
+++ src/content/FeaturesContent.js
@@ -1,14 +1,14 @@
 export const CONTENT = [
   {
     title: "Improved",
-    text: "XeroLinux runs fast due to the many, many fixes and under the hood tweaks that will save you time and provide you with a better & smoother experience"
+    text: "XeroLinux runs faster and better with many fixes and effort under the hood that will save you time and gives you better experience"
   },
   {
     title: "Stunning",
-    text: "XeroLinux looks Stunning out of the box. Using almost everything at our disposal from KDE making it look sexy without compromising performance. We also offer many alternate rices as well."
+    text: "XeroLinux looks great and is Stunning. Using almost everything at our disposal from KDE making it look sexy without compromising performance."
   },
   {
     title: "Powerful",
-    text: "With built-in support for AUR and Flatpaks, as well as the topgrade T.U.I updater, XeroLinux gives you the power to shape your Linux system YOUR way, the easy way."
+    text: "With built-in support for AUR (Paru), and being lightweight, XeroLinux gives you the power to do whatever you want on your Linux PC."
   }
 ]
