c0f4fea1d0322ba0785b2ad239f9dc0564b0833e
mhmdali102 <mhmdali102@bk.ru>
2022-08-13 04:07:19 +0300

    donate route

--- src/index.jsx
+++ src/index.jsx
@@ -6,12 +6,19 @@ import App from "./App";
 import ThemeContextProvider from "./contexts/ThemeContext";
 import PopUpContextProvider from "./contexts/PopUpContext";
 import ReleaseCountdownContextProvider from "./contexts/ReleaseCountdownContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Donate from "./components/Donate";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeContextProvider>
     <PopUpContextProvider>
       <ReleaseCountdownContextProvider>
-        <App />
+        <Router>
+          <Routes>
+            <Route path="/donate" element={<Donate />} />
+            <Route path="/" element={<App />} />
+          </Routes>
+        </Router>
       </ReleaseCountdownContextProvider>
     </PopUpContextProvider>
   </ThemeContextProvider>
