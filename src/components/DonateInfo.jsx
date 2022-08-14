de8b414daa08a5cbd3f388a08831a2a10b5814be
mhmdali102 <mhmdali102@bk.ru>
2022-08-14 22:50:30 +0300

    remove the close button add more space between buttons and add small image of the Steam Deck i the (i) box

--- src/components/DonateInfo.jsx
+++ src/components/DonateInfo.jsx
@@ -1,16 +1,17 @@
 import styled from "styled-components";
-import SteamDeckImg from "../assets/images/help.png";
+import SteamDeckImg from "../assets/images/steamdeck.png";
 
 const DonateInfo = () => {
   return (
     <Div>
       <Img src={SteamDeckImg} alt="Steam Deck Image" />
       <p>
-        <strong>- Help project to continue :</strong><br /><br />
-
-My country, <span>(Lebanon)</span> is currently going through a bad economic & political crisis. In light of the current situation, maintaining the project, or any extra ones, is harder than it should be. Your contributions will go a long way into sustaining it for a long time to come.<br /><br />
-
-<center>Support me on <span>Ko-Fi</span>, or Become a Github <span>Sponsor</span>.</center>
+        "We think that XeroLinux has finally reached its potential when it comes
+        to Desktops and laptops. So we have decided to target other devices,
+        like the new SteamDeck as we feel that SteamOS has too many
+        restrictions, and we want to unleash more power by giving you, the user
+        more freedom when it comes to adding features and using it as a portable
+        Desktop ;)"
       </p>
     </Div>
   );
