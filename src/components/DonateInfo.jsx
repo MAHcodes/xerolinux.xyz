98d008323f87ea49083b16e52222e37f26e0533c
mhmdali102 <mhmdali102@bk.ru>
2022-08-15 03:02:03 +0300

    fixes

--- src/components/DonateInfo.jsx
+++ src/components/DonateInfo.jsx
@@ -8,7 +8,7 @@ const DonateInfo = () => {
       <p>
         "We think that XeroLinux has finally reached its potential when it comes
         to Desktops and laptops. So we have decided to target other devices,
-        like the new SteamDeck as we feel that SteamOS has too many
+        like the new <span><a href="https://store.steampowered.com/steamdeck">SteamDeck</a></span> as we feel that SteamOS has too many
         restrictions, and we want to unleash more power by giving you, the user
         more freedom when it comes to adding features and using it as a portable
         Desktop ;)"
