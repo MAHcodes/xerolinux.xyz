288c657fefeb7e2ff5a4ecfdf144aae1e0eb8cc1
mhmdali102 <mhmdali102@bk.ru>
2022-10-16 18:13:56 +0300

    horizantal scroll message

--- src/index.jsx
+++ src/index.jsx
@@ -8,13 +8,21 @@ import ThemeContextProvider from "./contexts/ThemeContext";
 import PopUpContextProvider from "./contexts/PopUpContext";
 import ReleaseCountdownContextProvider from "./contexts/ReleaseCountdownContext";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_HYGRAPH_URI,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeContextProvider>
     <PopUpContextProvider>
       <ReleaseCountdownContextProvider>
         <Router>
-          <App />
+          <ApolloProvider client={client}>
+            <App />
+          </ApolloProvider>
         </Router>
       </ReleaseCountdownContextProvider>
     </PopUpContextProvider>
