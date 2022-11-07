43da35315b5d96976bc666aa00125a1a505328d9
XeroLinux <steve@techxero.com>
2022-11-07 13:29:37 +0200

    Added Fosstodon

--- src/content/HeroContent.js
+++ src/content/HeroContent.js
@@ -1,10 +1,10 @@
 export const CONTENT = {
-  heading: "XeroLinux",
-  text: `A Hobbyist Arch-Based Distro, built using
-              <a href="https://alci.online/" target="_blank" rel="noreferrer">
-                ArcoLinux's ALCI
-              </a>
-              scripts that is meant to provide you with a stable base, making it simpler to install ArchLinux <a href="https://calamares.io/" target="_blank" rel="noreferrer">
+  heading: "~{ XeroLinux }~",
+  text: `A Hobbyist Arch-Based Distro, built using in-house scripts created by your Friendly Human
+              <a href="https://github.com/keyaedisa" target="_blank" rel="noreferrer">
+                Keyaedisa
+              </a>,
+              a proud contributor to the project, that is meant to provide you with a stable base, making it simpler to install ArchLinux <a href="https://calamares.io/" target="_blank" rel="noreferrer">
                 (Calamares)</a>
                with a well optimized & Customized KDE Plasma, that you can shape YOUR way, the easy way... !
               <br />
