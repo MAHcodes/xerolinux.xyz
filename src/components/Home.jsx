509429b7597431402239d099a95a0f0d2c35396e
mhmdali102 <mhmdali102@bk.ru>
2022-12-02 17:02:46 +0200

    global snowfall

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,9 @@ import { ReleaseCountdownContext } from "../contexts/ReleaseCountdownContext";
 import ReleaseCountdown from "./ReleaseCountdown";
 import Download from "./Download";
 import Message from "./Message";
-import Donate from "./Donate";
+import styled from "styled-components";
+import Snowfall from "react-snowfall";
+import Settings from "../content/Settings.json";
 
 const Home = () => {
   const countdown = useContext(ReleaseCountdownContext);
@@ -18,12 +20,23 @@ const Home = () => {
       <Hero countdown={countdown} />
       <Message />
       {countdown ? <ReleaseCountdown countdown={countdown} /> : <Download />}
-      <Donate />
       <Features />
       <Customization />
       <FAQ faqs={faqsJSON} />
+      {Settings.snowfall ? (
+        <SnowfallContainer>
+          <Snowfall snowflakeCount={70} speed={[0.5, 2.5]} wind={[-0.5, 3.0]} />
+        </SnowfallContainer>
+      ) : null}
     </div>
   );
 };
+
+const SnowfallContainer = styled.div`
+  position: fixed;
+  inset: 0;
+  pointer-events: none;
+  z-index: 10100;
+`;
 
 export default Home;
