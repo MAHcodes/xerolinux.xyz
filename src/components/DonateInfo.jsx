01477634d89f5ee9ba00340e55b716a88a676866
XeroLinux <steve@techxero.com>
2022-12-07 14:04:27 +0200

    Donations

--- src/components/DonateInfo.jsx
+++ src/components/DonateInfo.jsx
@@ -9,7 +9,7 @@ const DonateInfo = () => {
         <strong>- Help project to continue :</strong><br /><br />
 
 
-My country, <span>(Lebanon)</span> is currently going through a bad economic & political crisis. In light of the current situation, maintaining the project is harder than it should be. Your contributions will go a long way into sustaining it for a long time to come.<br /><br />
+My country, <span>(Lebanon)</span> is currently going through a bad economic & political crisis. In light of the current situation, maintaining the project, or any extra ones, pro-bono, is harder than it should be. Your contributions will go a long way into sustaining it for a long time to come.<br /><br />
 
 Select from the options below, how you would like to support the project. Keep in mind that <span>FundRazr</span> and <span>PayPal</span> are one time donations while <span>Patreon</span> will be monthly recurring contributions with various tiers, unlocking early access to future projects & more...
       </p>
