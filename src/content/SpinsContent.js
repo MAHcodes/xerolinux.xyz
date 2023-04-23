4f22794088452e3be6c7ce305a08000ba0adc7eb
BrokenAnsible <brokenansible@proton.me>
2023-04-23 11:32:29 -0500

    Spins content drop

--- src/content/SpinsContent.js
+++ src/content/SpinsContent.js
@@ -1,24 +1,26 @@
 export const CONTENT = [
     //XeroG Box
     {
-        title: "XEROG (Gnome)",
-        image: "https://i.imgur.com/4B6IwBb.jpeg",
+        title: "XeroG (Gnome)",
+        image: "https://i.imgur.com/1pjR59G.png",
         alt: "XeroG",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc id cursus metus aliquam eleifend. Elit ut aliquam purus sit.",
-        buttonLeftText: "Click Me",
-        buttonRightText: "Click Me Too",
-        buttonLeftAction: "",
-        buttonRightAction: ""
+        text: "This Spin uses the Gnome Desktop, Clean & Optimized using a small collection of extensions to make your experience a pleasant one. We stuck to defaults as much as we could, not moving away too much from what Gnome was meant to be. Up to you to shape it to fit your needs.",
+        buttonLeftText: "Build It Now",
+        buttonRightText: "Donate For ISO",
+        buttonLeftAction: "https://github.com/xerolinux/xero_g_iso",
+        buttonRightAction: "https://ko-fi.com/post/XeroG-Gnome-Spin-Access-Granted-M4M5K7FO7",
+        detailsAction: "https://forum.xerolinux.xyz/thread-201.html"
     },
     //XeroXFCE Box
     {
-        title: "XEROXFCE",
-        image: "https://i.imgur.com/4B6IwBb.jpeg",
+        title: "XeroXFCE",
+        image: "https://i.imgur.com/IrEstqO.jpeg",
         alt: "XeroXFCE",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc id cursus metus aliquam eleifend. Elit ut aliquam purus sit.",
-        buttonLeftText: "Click Me",
-        buttonRightText: "Click Me Too",
-        buttonLeftAction: "",
-        buttonRightAction: ""
+        text: "This Spin uses the XFCE Desktop, Clean & Optimized for lower-end devices. We stuck to defaults as much as we could, not moving away too much from what XFCE's meant to be. We used elements only from XFCE. Up to you to shape it to fit your needs.",
+        buttonLeftText: "Build It Now",
+        buttonRightText: "Donate For ISO",
+        buttonLeftAction: "https://github.com/xerolinux/xero_xfce_iso",
+        buttonRightAction: "https://ko-fi.com/xerolinux",
+        detailsAction: ""
     }
 ]
