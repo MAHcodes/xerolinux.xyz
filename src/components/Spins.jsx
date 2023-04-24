8bcdcde59623ac0cd8c52f2cd06a686ee3076168
BrokenAnsible <brokenansible@proton.me>
2023-04-23 19:21:30 -0500

    Adding image carousel library and implementation.

--- src/components/Spins.jsx
+++ src/components/Spins.jsx
@@ -7,8 +7,9 @@ import { useState, useEffect } from "react";
 import SpinsIntro from "../content/SpinsIntro.md";
 import SpinsOutro from "../content/SpinsOutro.md";
 import { CONTENT } from "../content/SpinsContent";
-import Img from "./Img";
 import bg from "../assets/images/herobg.avif";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Spins= () => {
 
@@ -32,11 +33,51 @@ const Spins= () => {
           });
       }, []);
 
+    const images = [
+      {
+        img: "https://i.imgur.com/BHiW12e.png",
+
+      },
+      {
+        img: "https://i.imgur.com/1pjR59G.png",
+        alt: "text goes here"
+      },
+      {
+        img: "https://i.imgur.com/fAqRXtp.jpeg",
+        alt: "text goes here"
+      },
+      {
+        img: "https://i.imgur.com/IrEstqO.jpeg",
+        alt: "text goes here"
+      },
+      {
+        img: "https://i.imgur.com/Aq7gypv.png",
+        alt: "text goes here" 
+      }
+    ];
+
     return (
         <Spinning id="Home">
             <SpinsIntroSection>
-                <Img src="https://i.imgur.com/BHiW12e.png" alt="XeroG" maxWidth="50%" />
+
                 <div className="container">
+                <Carrot>
+                  <Carousel
+                    autoPlay={true}
+                    centerMode={true}
+                    width={960}
+                    infiniteLoop={true}
+                    labels={false}
+                    showThumbs={true}
+                    showArrows={true}
+                    showStatus={false}
+                    centerSlidePercentage={90}>
+                      {images.map((URL, index) => (
+                        <div className="slide">
+                          <img alt={URL.alt} src={URL.img} key={index} />
+                      </div>))}
+                  </Carousel>
+                </Carrot>  
                     <Box>
                         <P className="container markdown" dangerouslySetInnerHTML={{__html: marked.parse(introContent) }} />
                     </Box>
@@ -49,7 +90,6 @@ const Spins= () => {
                         <Cards>
                             <ImageCard
                                 title={CONTENT[0].title}
-                                image={CONTENT[0].image}
                                 text={CONTENT[0].text}
                                 btn1Action={CONTENT[0].buttonLeftAction}
                                 btn1Text={CONTENT[0].buttonLeftText}
@@ -59,7 +99,6 @@ const Spins= () => {
                             />
                             <ImageCard
                                 title={CONTENT[1].title}
-                                image={CONTENT[1].image}
                                 text={CONTENT[1].text}
                                 btn1Action={CONTENT[1].buttonLeftAction}
                                 btn1Text={CONTENT[1].buttonLeftText}
@@ -110,6 +149,14 @@ const Spinning = styled.div`
   background-position: bottom;
   background-repeat: no-repeat;
   background-size: cover;
+`;
+
+const Carrot = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const SpinsIntroSection = styled.div`
